7748d82a33880d5d60e1fe48829a324e8796440a
Pradeep <pradeepnailwal55@gmail.com>
2024-10-07 23:04:08 +0530

    KKKK

--- src/com/org/JaiMaaAmbay.java
+++ src/com/org/JaiMaaAmbay.java
@@ -3,7 +3,7 @@ package com.org;
 public class JaiMaaAmbay {
 
 	public static void main(String[] args) {
-		System.out.println("Jai Shree Ram");
+		System.out.println("Jai Shree Prabhu Ram");
 		System.out.println("Jai Shree Hanuman Ji Bhagwan ");
 		System.out.println("Jai Shree Hanuman Ji");
 
